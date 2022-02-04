fa759a7903825d82e1fd56da7af036c8e697b254
Andony Ng <andony.ng.11@gmail.com>
2022-02-04 17:07:43 +0100

    hh

--- Navidad/src/com/polonorte/Andony.java
+++ Navidad/src/com/polonorte/Andony.java
@@ -1,8 +1,8 @@
 package com.polonorte;
 
 /**
- * <h1>Clase creada para 1DAM TECHNOLOGIES</h1>
- * <i>Todos los derechos reservados<i>. 
+ * <h1>Clase creada para 1DAM TECHNOLOGIES</h1>hh
+ * <i>Todos los derechos reservados<i>. hh
  * @author Andony Ng
  * @version 1.0
  *
