eaeacb36e7229c3ba7b1a8ec388f8d83e6c6ddd7
MarioVDLP <MarioVDLP@gamil.com>
2022-02-04 16:39:45 +0100

    :V

--- Navidad/src/com/polonorte/MarioH.java
+++ Navidad/src/com/polonorte/MarioH.java
@@ -1,8 +1,16 @@
 package com.polonorte;
 /**
+ * Mei eu mollis albucius, ex nisl contentiones vix.
+ * Duo persius volutpat at, cu iuvaret epicuri mei.
+ * Duo posse pertinacia no, ex dolor contentiones mea.
+ * Nec omnium utamur dignissim ne. Mundi lucilius
+ * salutandi an sea, ne sea aeque iudico comprehensam.
+ * 
+ * 
  * MI CLASE PERSONA
  * @author MarioH 
  * @version 0.1
+ * 
  *
  */
 
