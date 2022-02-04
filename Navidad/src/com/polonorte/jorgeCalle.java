fba09d2d184aa145ac3eefb5f9a61e7ee95c3e60
jorgeCalle2212 <jorge.calle2212@gmail.com>
2022-02-04 16:37:59 +0100

    Se comenta la clase informando como funciona

--- Navidad/src/com/polonorte/jorgeCalle.java
+++ Navidad/src/com/polonorte/jorgeCalle.java
@@ -1,8 +1,8 @@
 package com.polonorte;
 
 /**
- * <h1>Clase creada para 1DAM TECHNOLOGIES</h1>
- * <i>Todos los derechos reservados<i>. 
+ * Clase crea objetos de tipo jorgeCalle
+ * con una serie de atributos
  * @author Jorge Calle Rodríguez
  *
  */
