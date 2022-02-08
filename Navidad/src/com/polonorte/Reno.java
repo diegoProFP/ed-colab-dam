0b33d1dbb9ba96a8e1afa6456ae7bc6db55ee016
Andony Ng <andony.ng.11@gmail.com>
2022-02-08 16:20:38 +0100

    intento de conflicto

--- Navidad/src/com/polonorte/Reno.java
+++ Navidad/src/com/polonorte/Reno.java
@@ -19,7 +19,7 @@ public class Reno {
      * @return El grito de guerra que devuelve el reno
      */
     public String volar(String direccion){
-        System.out.println("Reno.volar");
+        System.out.println("Reno.volarr");
 
         System.out.println("Reno volando");//comment
         return "mmmmmiiiiii chus chus peeeeerrrr";
