d61d12bf9d09d0c946c0a41be5d2f740872b6c77
1dama <brubraganca@gmail.com>
2022-02-08 16:33:15 +0100

    cambi de valores

--- Navidad/src/com/polonorte/Trineo.java
+++ Navidad/src/com/polonorte/Trineo.java
@@ -61,7 +61,7 @@ public class Trineo {
 	    
 	    if(numeroPeleas == 0) {
 	    	haSidoBueno = true;
-	    }else if(numeroPeleas > 0 && vecesGracias > 10 && vecesPorFavor > 8) {
+	    }else if(numeroPeleas > 0 && vecesGracias > 5 && vecesPorFavor > 8 && !nombre.equals("Chicho")) {
 	    	haSidoBueno = true;
 	    }else {
 	    	haSidoBueno = false;
